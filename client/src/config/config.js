12f9bc198ea3d553424e521d5c8f0fff663beed8
Abeldeboch1 <abeldeboch1@gmail.com>
2023-06-22 17:26:08 -0600

    update URL

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
   development: {
     // backendUrl: "http://localhost:8080/api/v1/dalle",
-      backendUrl: "https://react-three-js-9x4r.onrender.com/api/v1/dalle",
+    backendUrl: "https://react-three-js-9x4r.onrender.com/api/v1/dalle",
   },
   production: {
     backendUrl: "https://react-three-js-9x4r.onrender.com/api/v1/dalle",
